611e6ceee0af0fb15347dc21f815b6a7cb8f6406
Staubgeborener <eric.schroeder@protonmail.com>
2023-03-12 16:48:10 +0100

    beautify code

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,14 @@ export const mod = upgradeMod(modVanilla, features);
 main();
 
 function main() {
+  // Disable skip buttons
   mod.AddCallbackCustom(ModCallbackCustom.POST_NEW_ROOM_EARLY, () => {
     if (game.GetRoom().GetType() === RoomType.BOSS) {
       mod.disableAllInputs("disable-versus-screen-skip-key-input");
     }
   });
 
+  // Enable skip buttons
   mod.AddCallback(ModCallback.POST_UPDATE, () => {
     if (game.GetRoom().GetType() === RoomType.BOSS) {
       if (game.GetRoom().GetFrameCount() > 1) {
