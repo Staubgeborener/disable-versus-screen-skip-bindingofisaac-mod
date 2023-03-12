c95d0a36ad19829697932a8f6d783b423d445ab0
Staubgeborener <eric.schroeder@protonmail.com>
2023-03-12 14:00:13 +0100

    'let' to 'const'

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,9 @@ main();
 
 function main() {
   mod.AddCallback(ModCallback.POST_UPDATE, () => {
-    let roomData = Game().GetRoom().GetType();
+    const roomData = Game().GetRoom().GetType();
     if (roomData === RoomType.BOSS) {
-      let frameCount = Game().GetRoom().GetFrameCount();
+      const frameCount = Game().GetRoom().GetFrameCount();
       if (frameCount < 1) {
         mod.disableAllInputs("h34984h0fv");
       } else {
@@ -21,14 +21,10 @@ function main() {
     }
   });
 
-  function postGameStarted() {
-    Isaac.DebugString("Callback fired: POST_GAME_STARTED");
-  }
-
-  // just to check the current frame
+  // Just to check the current frame.
   mod.AddCallback(ModCallback.POST_RENDER, () => {
-    let frameCountY = Game().GetRoom().GetFrameCount();
-    let frameCountX = `${frameCountY}`;
+    const frameCountY = Game().GetRoom().GetFrameCount();
+    const frameCountX = `${frameCountY}`;
     Isaac.RenderText(frameCountX, 50, 50, 0.5, 0.5, 1, 1);
   });
 }
